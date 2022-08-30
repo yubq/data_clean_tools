00177c4b2eaadc3a9139433ca7133350d39e711b
yubq <741704471@qq.com>
2022-08-30 16:21:45 +0800

    更新

--- utils/util.py
+++ utils/util.py
@@ -42,8 +42,10 @@ from flask import request,send_from_directory
 from collections import Counter
 from elasticsearch import Elasticsearch
 from  utils.python2oracle import py2oracle
+from utils.batch_processing import search_data_batch
 
 es = Elasticsearch("")
+progress_dict = {}
 
 def clean_index(sql):
     pyor = py2oracle()
@@ -187,7 +189,7 @@ def data_topn(data):
     data = data.fillna("待补充")
     return data
 
-def cleen_upload(indexname,uppath):
+def clean_upload(indexname,uppath):
     pyor = py2oracle()
     if request.method == "POST":
         f = request.files['file']
@@ -197,3 +199,114 @@ def cleen_upload(indexname,uppath):
             pass
         if user is None:
             pass
+        if not os.path.exists(uppath):
+            os.mkdir(uppath)
+        upload_path = os.path.join(uppath,f.filename)
+        f.save(upload_path)
+        uptime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file_nama = f.filename
+        file_path = upload_path
+        try:
+            if file_nama[-3:] == 'csv':
+                data = pd.read_csv(file_path,encoding='gbk')
+            elif file_nama[-3:] == 'xls' or file_nama[-4:]:
+                data = pd.read_excel(file_path)
+            else:
+                return False
+        except Exception as e:
+            return False
+        data = data.drop_duplicates()
+        data = data.dropna()
+        data = data.reset_index(drop=True)
+        if data.columns[0] != '输入信息':
+            return "请下载模板"
+        elif len(data['输入信息']) == 0:
+            return "未检测到内容"
+        # 上传文件日志
+        sql = ""
+        t0 = time.time()
+        data_detail = []
+        data_nums = data.shape[0]
+        try:
+            for i in range(data_nums):
+                data.lic[i,['','','','']] = search_data_batch(data['输入信息'].loc[i])
+                num_progress = round((i+1)*50 / data_nums,2)
+                progress_dict[uuid] = num_progress
+            data = data_topn(data)
+            vaild_v = data[data[''] == '待补充'].shape[0]
+            progress_dict[uuid] = 60
+            t = time.time() - t0
+            sql2 = ""
+            for i in range(data.shape[0]):
+                data_detail.append(('','',''))
+            sql3 = "update "
+            insert_res = pyor.insert_data(sql3)
+            res_many = pyor.insert_manydata(sql2,data_detail)
+            response = True
+        except Exception as e:
+            response = e
+        return response
+
+def clean_data_progress():
+    uuid = request.args.get('uuid')
+    try:
+        response = {uuid:progress_dict[uuid]}
+    except Exception as e:
+        response = e
+    return response
+
+def clean_history(cleantype):
+    pyor = py2oracle()
+    try:
+        user = request.args.get('user')
+        filename = request.args.get('filename')
+        loaddate = request.args.get('loaddate')
+        page = request.args.get('page')
+        page_num = request.args.get('page_num')
+        if page_num is None:
+            page_num = 10
+        page_num = int(page_num)
+        sql = "根据业务场景设计sql逻辑"
+        history_res = pyor.search_data(sql)
+        if page is None:
+            page = 1
+        start = (int(page)-1)*page_num
+        end = int(page)*page_num
+        values = len(history_res)
+        if end >values:
+            end = values
+        history_res = history_res[start:end]
+        response = history_res
+
+    except Exception as e:
+        response = e
+    return response
+
+# 覆盖原sheet页追加问价
+def file_append(index_name,user,file_name,data):
+    file_path =os.getcwd()
+    down_date = str(datetime.date.today()).replace('-','')
+    clean_type = 'download_file'
+    down_path = '场景文件名'
+    tempfile_name = '文件名'
+    sheet_name = '清洗结果'
+    file_path = os.path.join(file_path,clean_type,down_path,down_date,user)
+    if not os.path.exists(file_path)
+        os.mkdir(file_path)
+    ori_path = os.path.join(os.getcwd(),'文件名',tempfile_name)
+    file_name = file_name.replace('csv','xlsx')
+    copy_path = os.path.join(file_path,file_name)
+    shutil.copy(ori_path,copy_path)
+    writer = pd.ExcelWriter(copy_path,engine='openpyxl')
+    writer_book = openpyxl.load_workbook(writer.path)
+    writer.book = writer_book
+    idx = writer_book.sheetnames.index(sheet_name)
+    writer_book.remove(writer_book.worksheets[idx])
+    writer_book.create_sheet(sheet_name+'结果',idx)
+    writer.sheets = dict((i.title,i) for i in writer_book.worksheets)
+    data.to_excel(writer,sheet_name+'结果',index=False,encoding='utf8')
+    writer.save()
+    return file_path,file_name
+
+def files
+
